2a8a5ea4b8c2b22f629e914f1b8867a574419ecc
mejxo <miklovicmaxo@gmail.com>
2024-06-13 11:48:49 +0200

    update

--- projekt/script/script.js
+++ projekt/script/script.js
@@ -1,12 +1,12 @@
 function add() {
-    var message = "The title with these data has been entered into the database\n";
-    var title = "Title: " + document.getElementById('name').value + "\n";
-    var author = "Author: " + document.getElementById('author').value + "\n";
-    var copies = "Available Copies: " + document.getElementById('copies').value + "\n";
-    var pages = "Number of Pages: " + document.getElementById('pages').value + "\n";
-    var isbn = "ISBN: " + document.getElementById('isbn').value + "\n";
-    var price = "Price: " + document.getElementById('price').value;
+    let message = "The title with these data has been entered into the database\n";
+    let title = "Title: " + document.getElementById('name').value + "\n";
+    let author = "Author: " + document.getElementById('author').value + "\n";
+    let copies = "Available Copies: " + document.getElementById('copies').value + "\n";
+    let pages = "Number of Pages: " + document.getElementById('pages').value + "\n";
+    let isbn = "ISBN: " + document.getElementById('isbn').value + "\n";
+    let price = "Price: " + document.getElementById('price').value;
 
-    var result = message + title + author + copies + pages + isbn + price;
+    let result = message + title + author + copies + pages + isbn + price;
     alert(result);
 }
