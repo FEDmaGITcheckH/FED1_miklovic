9b7b27734690ab28dcebc900a23efc466d471ee8
mejxo <miklovicmaxo@gmail.com>
2024-06-19 09:20:03 +0200

    12.3.1

--- projekt/script/script.js
+++ projekt/script/script.js
@@ -1,4 +1,4 @@
-function add() {
+/*function add() {
     let message = "The title with these data has been entered into the database\n";
     let title = "Title: " + document.getElementById('name').value + "\n";
     let author = "Author: " + document.getElementById('author').value + "\n";
@@ -9,4 +9,50 @@ function add() {
 
     let result = message + title + author + copies + pages + isbn + price;
     alert(result);
+}*/
+
+let books = [];
+
+function add(event) {
+    event.preventDefault();
+    
+    let book = {
+        title : document.getElementById('name').value,
+        author : document.getElementById('author').value,
+        copies : document.getElementById('copies').value,
+        pages : document.getElementById('pages').value,
+        isbn :  document.getElementById('isbn').value,
+        price : document.getElementById('price').value,
+    };
+
+    books.push(book);
+    write();
+}
+
+function deleteLast() {
+    books.pop();
+    write();
+}
+
+function deleteAll() {
+    books = [];
+    write();
+}
+
+function write() {
+    console.clear();
+    /*console.log(".......................................................");
+    console.log("Books in LMS:");
+    console.log(".......................................................");*/
+
+    for(let i = 0; i < books.length; i++) {
+        console.log("Book " + (i+1));
+        console.log("Author: " + books[i].author);
+        console.log("Title: " + books[i].title);
+        console.log("Avaible copies: " + books[i].copies);
+        console.log("Number of Pages: " + books[i].pages);
+        console.log("ISBN: " + books[i].isbn);
+        console.log("Price of Book: " + books[i].price + " Euro");
+        console.log("_______________________________________________________");
+    }
 }
